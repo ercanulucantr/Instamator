a8da803e9904a0c959d38cd573f5e5ae404e7748
Ercan Ulucan <nacreif@gmail.com>
2019-02-03 00:45:23 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
-// Kullanıcı Bilgileriniz
+#######################
+# KULLANICI BİLGİLERİ #
+#######################
 $account = array(
     'username' => '', // Kullanıcı adınız
     'password' => '' // Şifreniz
 );
-
-
 // Kullanıcı Bilgisi Boş/Dolu Kontrolü
 if($account['username'] == '' || $account['password'] == '')
 {
@@ -13,9 +13,16 @@ if($account['username'] == '' || $account['password'] == '')
 }
 
 
+
+
+######################
+# OTOMASYON AYARLARI #
+######################
+
 // Zaman Tüneli Beğeni Ayarları
 $timeline_liker = array(
-    'interval' => 20 // Her beğeni sonrası beklenecek süre (Minimum Saniye: 20)
+    'interval' => 20, // Her beğeni sonrası beklenecek süre (Minimum Saniye: 20)
+	'have_err' => 120 // Her hangi bir hata alınır ise beklenecek süre (Minimum Saniye: 120)
 );
 
 
@@ -23,5 +30,6 @@ $timeline_liker = array(
 $timeline_commenter = array(
     'is_likes' => 0, // Yorum atılan gönderilerin aynı zamanda beğenilmesini istiyorsanız değeri 1 yapın.
     'interval' => 50, // Her yorum sonrası beklenecek süre (Minimum Saniye: 50)
+	'have_err' => 120, // Her hangi bir hata alınır ise beklenecek süre (Minimum Saniye: 120)
     'comments' => array('omg', 'cool', 'wow') // Gönderilere atılacak yorumları yazın
 );
